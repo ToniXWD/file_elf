098ff51034f1c9c29224d6262501c7fcbeac767d
ToniXWD <807077266@qq.com>
2024-09-12 06:22:22 +0800

    0.4: 补充资源文件

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    // println!("cargo:rustc-link-search=native=C:/Users/toni/miniconda3/Library/lib");
-    // println!("cargo:rustc-link-lib=static=sqlite3");
+    println!("cargo:rustc-link-search=native=C:/Users/toni/miniconda3/Library/lib");
+    println!("cargo:rustc-link-lib=static=sqlite3");
 }
